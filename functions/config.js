5e5b63b854ae5d47fd6e39fa9709d5c414681e0a
Wade Armstrong <wade@wadearmstrong.com>
2017-12-27 21:19:12 -0800

    Refactor for configurability
    * Break out config into external file
    * Have config'd random #s determine what's going on
    * Have slowness, if available, determined by config as well

---
+++ functions/config.js
@@ -0,0 +1,11 @@
+/**
+ * failureLevel is what times out of 10 you want it to fail
+ * annoyanceLevel is, out of 10:
+ * 		what # of times it doesn't fail you want it to be slow
+ * 		how slow it is when it's slow
+ */
+
+module.exports = {
+	failureLevel: 3,
+	annoyanceLevel: 6,
+};
--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,30 @@
 const functions = require('firebase-functions');
+const config = require('./config');
 
+
+const outcomes = {
+	ERROR: 'error',
+	SLOW: 'slow',
+	NORMAL: true,
+};
 
 function slowen() {
-	return new Promise(resolve => setTimeout(resolve, 1000));
+	let howSlow = Math.floor(Math.random() * 1000 * config.annoyanceLevel);
+
+	return new Promise(resolve => setTimeout(resolve, howSlow));
+}
+
+function determineOutcome() {
+	let rando = Math.floor(Math.random() * 10);
+
+	if (rando < config.failureLevel) return outcomes.ERROR;
+	if (rando < config.annoyanceLevel) return outcomes.SLOW;
+	return outcomes.NORMAL;
 }
 
 exports.slowifyApp = functions.https.onRequest((req, res) => {
 	// determine if we're doing nothing, a delay, or an error
-	const which = req.query.which;
+	const which = determineOutcome();
 
 	switch (which) {
 		case 'slow':
@@ -20,9 +37,3 @@ exports.slowifyApp = functions.https.onRequest((req, res) => {
 			res.status(200).end('Success (ish)');
 	}
 });
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
